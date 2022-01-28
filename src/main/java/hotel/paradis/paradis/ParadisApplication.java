5161498eeb3c944378c1681a6d2ce2e7e1c641f9
gomez-julian <18161126@itoaxaca.edu.mx>
2022-01-28 00:22:52 -0600

    Creación del proyecto

---
+++ src/main/java/hotel/paradis/paradis/ParadisApplication.java
@@ -0,0 +1,13 @@
+package hotel.paradis.paradis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParadisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParadisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hotel/paradis/paradis/ParadisApplicationTests.java
@@ -0,0 +1,13 @@
+package hotel.paradis.paradis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParadisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
